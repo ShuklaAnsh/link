9441ba37a97cc94b10ebbab4c1c7a7825f812649
Ansh Shukla <shuklaansh96@gmail.com>
2020-09-21 13:31:12 -0700

    formidable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import {FileInput, Button} from '@blueprintjs/core';
 import './App.css';
 
-function App() {
+export const App = () => {
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log(event)
+    const form: HTMLFormElement = document.getElementById(
+      "file-upload-form"
+    ) as HTMLFormElement;
+    const resp = await fetch("/api/file", {
+      method: "post",
+      body: new FormData(form),
+    });
+    console.log(resp)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <form id={"file-upload-form"} onSubmit={e => handleSubmit(e)}>
+          <FileInput />
+          <Button type={"submit"} />
+        </form>
     </div>
   );
 }
-
-export default App;
--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import express, { Express } from "express";
 import cors from "cors";
-import compression from "compression";  // compresses requests
+import compression from "compression";
+import formidable from "formidable";
 import session from "express-session";
 import bodyParser from "body-parser";
 import path from "path";
+import fs from "fs";
 
 // Create Express server
 const app: Express = express();
@@ -12,8 +14,35 @@ const PORT: string | number = process.env.PORT || 4000
 app.set("port", PORT);
 
 // Configuration
-app.use(cors())
+if(app.get('env') === 'development') app.use(cors());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'frontend')));
+
+
+//routes
+app.post('/api/file', (req, res, next) => {
+    try {
+        const form = new formidable.IncomingForm();
+        form.uploadDir = path.join(__dirname, '/uploads');
+        form.keepExtensions = true;
+        let files = [], fields = [];
+        form.on('field', function(field, value) {
+            fields.push([field, value]);
+        })
+        form.on('file', function(field, file) {
+            console.log(file.name);
+            files.push([field, file]);
+        })
+        form.on('end', function() {
+            console.log('done');
+        });
+        form.parse(req);
+        res.status(200).send("Uploaded");
+    } catch (e) {
+        console.log(e)
+        res.send("done")
+    }
+})
 
 // Start server
 app.listen(PORT, () => {
